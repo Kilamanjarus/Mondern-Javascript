c13055dd1c1fad28eaf755b9e33ba85bca1ebeaa
max <max.lillie95@gmail.com>
2022-08-19 12:43:22 -0500

    Added and tested moment, tested parser, created webpack for app and linked html

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+var Parser = require('expr-eval').Parser;
 console.log('hello i am in the js file')
 
-console.log(dayjs().format()) 
+var parser = new Parser();
+var expr = parser.parse('2 * x + 1');
+console.log(expr.evaluate({ x: 3 }));
+
+console.log(Parser.evaluate('6 * x', { x: 7 })) // 42
+
+
+const dayjs = require('dayjs')
+console.log(dayjs().format())
+
+const moment = require('moment')
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
